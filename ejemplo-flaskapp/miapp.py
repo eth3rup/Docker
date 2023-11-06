5cfb162863bad757e156e6a146c2ad48b2529249
José Carlos Gallego <jgallegocano@educantabria.es>
2023-11-06 17:54:19 +0100

    Update miapp.py

--- ejemplo-flaskapp/miapp.py
+++ ejemplo-flaskapp/miapp.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hola Mundo'
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
